5328769a8ab4597fc434cf3165f61511585b6308
rmayankbite <rmayank@vt.edu>
2026-09-18 13:45:30 -0400

    Refactor printDealerCards method for better formatting

--- collections/Dealer.java
+++ collections/Dealer.java
@@ -37,15 +37,66 @@ public class Dealer {
 
 
     public void printDealerCards(boolean hideCards) {
-        System.out.print(dealerHand.getCard(0).getSuitAndRank() + " ");
-        for (int i = 1; i < dealerHand.getDeckLength(); i++) {
-            if (hideCards) {
-                System.out.print("__ ");
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            System.out.print("┌──────────┐  ");
+        }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            if (i == 0 || !hideCards) {
+                Card card = dealerHand.getCard(i);
+                System.out.printf("│ %-9s│  ", card.getRank());
             }
             else {
-                System.out.print(dealerHand.getCard(i).getSuitAndRank() + " ");
+                System.out.print("│          │  ");
             }
         }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            System.out.print("│          │  ");
+        }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            if (i == 0 || !hideCards) {
+                Card card = dealerHand.getCard(i);
+                System.out.printf("│     %s    │  ", card.getSuit());
+            }
+            else {
+                System.out.print("│     ?    │  ");
+            }
+        }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            System.out.print("│          │  ");
+        }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            if (i == 0 || !hideCards) {
+                Card card = dealerHand.getCard(i);
+                System.out.printf("│        %-2s│  ", card.getRank());
+            }
+            else {
+                System.out.print("│          │  ");
+            }
+        }
+        System.out.println();
+
+       
+        for (int i = 0; i < dealerHand.getDeckLength(); i++) {
+            System.out.print("└──────────┘  ");
+        }
+        System.out.println();
     }
 
 
